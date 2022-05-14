b5455a78235c2f7ea8f37b06cebfc9f2496ff76c
xiangyun <xindubawukong@gmail.com>
2022-05-13 18:40:17 -0700

    add init_skeches3

--- IM.hpp
+++ IM.hpp
@@ -23,7 +23,8 @@ public:
         m = G.m;
     }
     void init_sketches(size_t R, bool parallel);
-    void init_sketches2(size_t R, bool parallel);
+    void init_sketches2(size_t R);
+    void init_sketches3(size_t R);
     pair<sequence<NodeId>, float> select_seeds(int k, size_t R);
 };
 
@@ -49,9 +50,8 @@ void InfluenceMaximizer::init_sketches(size_t R, bool parallel){
     }
 }
 
-void InfluenceMaximizer::init_sketches2(size_t R, bool parallel) {
+void InfluenceMaximizer::init_sketches2(size_t R) {
   cout << "init_sketches2" << endl;
-  assert(parallel);  // must parallel
   auto find = gbbs::find_variants::find_compress;
   auto splice = gbbs::splice_variants::split_atomic_one;
   auto unite =
@@ -73,8 +73,51 @@ void InfluenceMaximizer::init_sketches2(size_t R, bool parallel) {
       float w = G.W[j];
       parallel_for(0, R, [&](size_t k) {
         if (hash_edges[k](u, v, w)) {
+        // if (rand() < RAND_MAX * w) {
           unite(u, v, label[k]);
         }
+      });
+    });
+  });
+  skeches = sequence<sequence<size_t>>(R, sequence<size_t>(G.n));
+  parallel_for(0, R, [&](size_t k) {
+    auto& parents = skeches[k];
+    parallel_for(0, G.n,
+                 [&](size_t i) { parents[i] = (size_t)find(i, label[k]); });
+    auto hist = histogram_by_key(parents);
+    parallel_for(0, hist.size(), [&](size_t i) {
+      size_t node = hist[i].first;
+      size_t size = hist[i].second;
+      parents[node] = TOP_BIT | size;
+    });
+  });
+}
+
+void InfluenceMaximizer::init_sketches3(size_t R) {
+  cout << "init_sketches3" << endl;
+  auto find = gbbs::find_variants::find_compress;
+  auto splice = gbbs::splice_variants::split_atomic_one;
+  auto unite =
+      gbbs::unite_variants::UniteRemCAS<decltype(splice), decltype(find),
+                                        find_atomic_halve>(find, splice);
+  sequence<sequence<NodeId>> label(R, sequence<NodeId>(G.n));
+  parallel_for(0, R, [&](size_t i) {
+    parallel_for(0, G.n, [&](size_t j) { label[i][j] = j; });
+  });
+  sequence<Hash_Edge> hash_edges(R);
+  for (size_t i = 0; i < R; i++) {
+    hash_edges[i].graph_id = i;
+    hash_edges[i].forward = true;
+  }
+  parallel_for(0, G.n, [&](size_t i) {
+    NodeId u = i;
+    parallel_for(G.offset[i], G.offset[i + 1], [&](size_t j) {
+      NodeId v = G.E[j];
+      float w = G.W[j];
+      parallel_for(0, R * w + 2, [&](size_t k) {
+        auto t = _hash(_hash(u)+v)+_hash(k);
+        auto id = t % R;
+        unite(u, v, label[id]);
       });
     });
   });
