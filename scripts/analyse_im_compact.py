e5293e2299d85d1b301844eb87ef7b66f5575c43
xiangyun <xindubawukong@gmail.com>
2022-06-16 23:44:36 -0700

    draw figures

--- scripts/analyse_im_compact.py
+++ scripts/analyse_im_compact.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 
@@ -39,7 +40,13 @@ def analyse_im():
 
     memory_gb = list(map(lambda x: x / 1000000.0, memory_kb))
 
-    return init_time, seed_time, total_time, memory_kb, memory_gb
+    return {
+        'init_time': init_time,
+        'seed_time': seed_time,
+        'total_time': total_time,
+        'memory_kb': memory_kb,
+        'memory_gb': memory_gb,
+    }
 
 
 def analyse_compact(compact):
@@ -71,7 +78,38 @@ def analyse_compact(compact):
 
     memory_gb = list(map(lambda x: x / 1000000.0, memory_kb))
 
-    return init_time, seed_time, total_time, memory_kb, memory_gb
+    return {
+        'init_time': init_time,
+        'seed_time': seed_time,
+        'total_time': total_time,
+        'memory_kb': memory_kb,
+        'memory_gb': memory_gb,
+    }
+
+
+def draw_figure(data, file):
+    print(f'Generating {file}')
+    import matplotlib.pyplot as plt
+    fig = plt.figure(figsize=(12, 8))
+    plt.title(file)
+    xs = []
+    ys = []
+    for k, v in data.items():
+        if type(k) == float:
+            xs.append(k)
+            ys.append(v)
+    plt.plot(xs, ys, marker='o')
+    plt.hlines(data['im'], 0.0, 1.0, colors = "r", linestyles = "dashed")
+    
+    bottom, _ = plt.ylim()
+    if bottom < 0:
+        plt.ylim(bottom = 0)
+    plt.xticks([0.05, 0.1, 0.2, 0.3, 0.5, 1.0])
+    yticks = list(plt.yticks()[0])
+    yticks.append(data['im'])
+    plt.yticks(yticks)
+    plt.savefig(file)
+    plt.close(fig)
 
 
 def main():
@@ -80,14 +118,56 @@ def main():
     for compact in [0.05, 0.1, 0.2, 0.3, 0.5, 1.0]:
         data[compact] = analyse_compact(compact)
     print(data.keys())
+
     with open('excel.txt', 'w') as f:
         for i in range(21):
             line = []
             for p in data.values():
-                for l in p:
+                for l in p.values():
                     line.append(str(l[i]))
             line.append('\n')
             f.write(' '.join(line))
+
+    graphs = [
+        'HepPh_sym',
+        'Epinions_sym',
+        'Slashdot_sym',
+        'DBLP_sym',
+        'Youtube_sym',
+        'Orkut_sym',
+        'LiveJournal_sym',
+        'Twitter_sym',
+        'Friendster_sym',
+        'Sd_arc',
+        'USA_sym',
+        'GE_sym',
+        'HT-5',
+        'HH-5_sym',
+        'Ch-5_sym',
+        'GL-5_sym',
+        'COS-5_sym',
+        'SQR_sym',
+        'SQR-S_sym',
+        'REC_sym',
+        'REC-S_sym',
+    ]
+    columns = [
+        'init_time',
+        'seed_time',
+        'total_time',
+        'memory_kb',
+        'memory_gb',
+    ]
+    for i in range(len(graphs)):
+        path = f'./figures/{str(i).zfill(2)}_{graphs[i]}'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        for j in range(len(columns)):
+            file = path + '/' + columns[j] + '.jpg'
+            d = {}
+            for k, v in data.items():
+                d[k] = v[columns[j]][i]
+            draw_figure(d, file)
 
 
 if __name__ == '__main__':
